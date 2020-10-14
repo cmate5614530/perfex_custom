307f96303b938bb0a675dca657f2fc541c2062ef
cmate <cmate5614530@gmail.com>
2020-10-15 00:56:23 +0300

    initial

---
+++ aa_aa.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Test Page</title>
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,600&display=swap" rel="stylesheet">
+<style>
+body {
+	font-family: 'Montserrat', sans-serif;
+	text-align: center;
+}
+.testcontainer {
+	width: 100%;
+	float: left;
+}
+.testcontent {
+	width: 700px;
+	margin: 0 auto;
+	border: 1px solid #ccc;
+	padding-bottom: 25px;
+	margin-top: 100px;
+}
+h1 {
+	background-color: #CCCCFF;
+	padding: 20px;
+	margin-top: 0px;
+}
+h5 {
+	color: #6C0;
+	font-size: 16px;
+}
+span {
+	text-decoration: underline;
+	color: #000;
+}
+.addpadding {
+	padding-top: 10px;
+}
+</style>
+</head>
+</html>
